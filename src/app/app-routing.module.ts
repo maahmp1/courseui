2a450c87e80c4c49ee774eb2fc856896fb0691e4
Marcela Martins <marcelamartinspena@gmail.com>
2022-10-07 08:49:46 -0300

    added routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosModule } from './cursos/cursos.module';
 
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  {
+    path: 'cursos', loadChildren: () =>import('./cursos/cursos.module').then(m => m.CursosModule)
+  },
+  {
+    path:'alunos', loadChildren: () =>import('./alunos/alunos.module').then(m => m.AlunosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListacursosComponent } from './listacursos/listacursos.component';
+import { ListaalunosComponent } from '../alunos/listaalunos/listaalunos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ListacursosComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
